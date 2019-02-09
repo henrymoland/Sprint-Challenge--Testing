3c98e5b92db828a88d8528a87cd9a4e494ac3991
Henry Moland <molandh@gmail.com>
2019-02-09 10:27:53 -0800

    created endpoints for POST, GET and DELETE and sever test file to test enpoints

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     connection: {
       filename: './data/dev.sqlite3'
     },
-    UseNullAsDefault: true,
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
     },
