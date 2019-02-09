c8bc63982f3c5fdd2b063b070c2fb19f93ae0817
Henry Moland <molandh@gmail.com>
2019-02-09 09:41:51 -0800

    Knex install, SQLite3 install and migration

---
+++ data/migrations/20190209093855_games.js
@@ -0,0 +1,14 @@
+
+// Create a games table with title, genre and release year
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('games', tbl => {
+        tbl.increments();
+        tbl.string('title', 240).notNullable().unique();
+        tbl.string('genre', 120).notNullable();
+        tbl.integer('releaseYear').notNullable();
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('games');
+}
